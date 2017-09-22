c3262d3fed813c9894d96fbee95ff7b257dfe786
hongyun.mhy <hongyun.mhy@alibaba-inc.com>
2017-09-22 10:51:24 +0800

    init commit

---
+++ app/src/main/java/cookbook/test_mux_encode/MainActivity.java
@@ -0,0 +1,13 @@
+package cookbook.test_mux_encode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
